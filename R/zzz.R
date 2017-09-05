465a5511a760ed44d559f377d6463360e13728f8
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2017-09-04 23:32:49 -0600

    on attach message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname = find.package("MarginalMediation"), pkgname = "MarginalMediation") {
+  packageStartupMessage("MarginalMediation 0.3.0: This is beta software.\nPlease report any bugs (t.barrett@aggiemail.usu.edu).")
+}
