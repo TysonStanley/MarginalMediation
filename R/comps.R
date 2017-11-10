046d6ddf88cdbe50f293350479be9478b673e2db
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2017-11-10 00:41:38 -0700

    rel_effects function

--- R/comps.R
+++ R/comps.R
@@ -1,16 +1,16 @@
-#' Indirect vs Direct Comparison
+#' Indirect Effect vs Total Effect
 #' 
 #' To make comparisons between indirect and direct effects of a variable.
-#' The default comparison is indirect / direct.                                                                                                             
+#' The default comparison is indirect / (direct + indirect)                                                                                                             
 #' 
 #' @param model mma fit object
-#' @param variable the variable in the model that is to be compared
+#' @param effect the indirect effect to be compared to its direct path
 #' 
 #' @export
-rel_effects = function(model, variable){
-  ind = model$ind_effects
-  dir = model$dir_effects
+rel_effects = function(model, effect){
+  ind = model$ind_effects[effect, 3]
+  dir = model$dir_effects[gsub("-.*$", "", effect), 1]
   
-  ind[variable, ]/dir[variable,]
+  ind/(dir + ind)
 }
 
